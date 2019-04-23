19209221f7632871651b45e56b026214d20f6f9f
Andrej Savikin <andrejsavikin@gmail.com>
2019-04-23 22:49:12 +0200

    Sredjena struktura fajlova

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 import Adafruit_DHT
-app = Flask(__name__)
+app = Flask(__name__, root_path="html")
 @app.route('/')
 def kurton():
     humidity, temperature =penis()
