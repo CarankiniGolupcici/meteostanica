295529cb98e2118cdb1cebc7130836a845e7f7d0
Andreja <andrejastambolija@gmail.com>
2019-05-07 04:21:30 -0700

    razredna volimvas

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask
-from flask import render_template
-import Adafruit_DHT
-app = Flask(__name__, root_path="html")
+import os
+
+from flask import Flask, url_for, render_template
+import 
+app = Flask(__name__, root_path=os.path.join(os.path.dirname(__file__),"html"))
 @app.route('/')
 def kurton():
     humidity, temperature =penis()
     return render_template('sajt.html', temperatura=temperature, vazduh=humidity, pritisak=35)
 def penis():
-    sensor = Adafruit_DHT.AM2302
+    sensor = 3 #Adafruit_DHT.AM2302
     pin= 1
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    #humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    humidity, temperature = 50, 3
+    print(url_for('static', filename='background.png'))
     if humidity is not None and temperature is not None:
         return humidity, temperature
     else:
         return 0,0
+
+
+if __name__ == '__main__':
+    app.run()
