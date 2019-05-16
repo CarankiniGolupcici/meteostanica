a830440f7cb26eca0d9d94fc74a3b7f5a4a19500
Andreja <andrejastambolija@gmail.com>
2019-05-16 09:48:07 -0700

    kuracpickadupesisa

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 
-from flask import Flask, url_for, render_template
-
-#import senzori
-#sensor_helper = senzori.senzori()
+from flask import Flask, render_template
+import mysql.connector
+# import senzori
+# sensor_helper = senzori.senzori()
 import senzori_test
+
 sensor_helper = senzori_test.senzori()
 
 app = Flask(__name__, root_path=os.path.join(os.path.dirname(__file__), "html"))
@@ -14,6 +15,7 @@ app = Flask(__name__, root_path=os.path.join(os.path.dirname(__file__), "html"))
 def kurton():
     humidity, temperature = sensor_helper.getHumidityAndTemp()
     return render_template('sajt.html', temperatura=temperature, vazduh=humidity, pritisak=35)
+
 
 
 if __name__ == '__main__':
