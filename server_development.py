7315f078023bb46effadb222615c90da22732485
Andrej Savikin <andrejsavikin@gmail.com>
2019-05-20 12:32:34 +0200

    Fixed test entries

--- server_development.py
+++ server_development.py
@@ -8,10 +8,10 @@ app = Flask(__name__, root_path=os.path.join(os.path.dirname(__file__),"html"))
 def devserver(page):
     print("Page: ")
     print(page)
-    entries = [{temperatura_1:50, temperatura_2:49, pritisak:500, vlaznost:48},
-               {temperatura_1: 47, temperatura_2: 46, pritisak: 501, vlaznost: 45},
-               {temperatura_1: 44, temperatura_2: 43, pritisak: 502, vlaznost: 42},
-               {temperatura_1: 41, temperatura_2: 40, pritisak: 503, vlaznost: 39}]
+    entries = [{'temperatura_1':50, 'temperatura_2':49, 'pritisak':500, 'vlaznost':48},
+               {'temperatura_1': 47, 'temperatura_2': 46, 'pritisak': 501, 'vlaznost': 45},
+               {'temperatura_1': 44, 'temperatura_2': 43, 'pritisak': 502, 'vlaznost': 42},
+               {'temperatura_1': 41, 'temperatura_2': 40, 'pritisak': 503, 'vlaznost': 39}]
 
     if page == "":
         return render_template('sajt.html', temperatura=30, vazduh=30, pritisak=35, entries=entries)
