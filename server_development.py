feca47cc9f6cd0ea7730144aeead3771ab6b0a92
Andrej Savikin <andrejsavikin@gmail.com>
2019-05-20 11:44:16 +0200

    Development server

--- server_development.py
+++ server_development.py
@@ -2,9 +2,21 @@ from flask import Flask
 import os
 from flask import render_template
 app = Flask(__name__, root_path=os.path.join(os.path.dirname(__file__),"html"))
-@app.route('/')
-def devserver():
-    return render_template('sajt.html', temperatura=30, vazduh=30, pritisak=35)
-	
+
+
+@app.route('/<page>')
+def devserver(page):
+    print("Page: ")
+    print(page)
+
+    if page == "":
+        return render_template('sajt.html', temperatura=30, vazduh=30, pritisak=35)
+    else:
+        if page.endswith(".html"):
+            return render_template(page, temperatura=30, vazduh=30, pritisak=35)
+        else:
+            return "404"
+
+
 if __name__ == "__main__":
-	app.run()
+    app.run()
